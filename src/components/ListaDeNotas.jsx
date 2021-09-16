64b33b9d133be34ffd99ee7e24dc6370a165852b
GhaSilva <ghabriel-silva@outlook.com>
2021-09-16 20:35:52 -0300

    Criando componentes - ListaDeNotas

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+ export class ListaDeNotas extends Component{
+    render(){
+        return(<ul>
+            <li>
+            <section>
+                <header>
+                <h3>Título</h3>
+                <p>Escreva sua nota</p>
+                </header>
+            </section>
+            </li>
+            <li>
+            <section>
+                <header>
+                <h3>Título</h3>
+                <p>Escreva sua nota</p>
+                </header>
+            </section>
+            </li>
+            <li>
+            <section>
+                <header>
+                <h3>Título</h3>
+                <p>Escreva sua nota</p>
+                </header>
+            </section>
+            </li>
+        </ul>)
+    }
+}
